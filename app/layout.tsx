8f1199de77206b0945ed45b77e550da6a938ee4d
周湧秝 <112405214@g.nccu.edu.tw>
2026-04-06 21:59:28 +0800

    fix:font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DynamicBackground from "../components/DynamicBackground";
 import Sidebar from "../components/Sidebar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "周湧秝的個人履歷",
@@ -25,11 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en" // 🌟 關鍵修正：改回 "en"，強制瀏覽器優先使用 Geist 英文字體
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-    >
-      <body className="font-sans text-gray-800 md:h-screen p-4 md:p-8 md:overflow-hidden flex flex-col relative z-0">
+    <html lang="en" className="antialiased">
+      {/* 🌟 已經將 font-sans 移除，讓等一下的 globals.css 直接接管字體 */}
+      <body className="text-gray-800 md:h-screen p-4 md:p-8 md:overflow-hidden flex flex-col relative z-0">
         
         <DynamicBackground />
         
